65920545ce902e19a621fefc15f6d2e2040f920a
Your Name <you@example.com>
2021-10-16 23:56:50 -0500

    First Commit

---
+++ MvcMovie/Controllers/PersonController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            if (Session["Person"] == null)
+            {
+                List<Person> person = new List<Person>();
+                person.Add(new Person { ID = 1, FirstName= "Ruth", LastName="Carbajal", BirthDay=new DateTime(2002,05,03), IsTecsup=true});
+                person.Add(new Person { ID = 2, FirstName = "Camila", LastName = "Sanchez", BirthDay = new DateTime(2000, 10, 27), IsTecsup = false });
+                Session["Person"] = person;
+            }
+            return View(Session["Person"]);
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = ((List<Person>)Session["Person"]).Where(x => x.ID == id).FirstOrDefault();
+
+            return View(model);
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(Person model)
+        {
+            try
+            {
+                if (Session["Person"] == null)
+                    Session["Person"] = new List<Person>();
+                // TODO: Add insert logic here
+                ((List<Person>)Session["Person"]).Add(model);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = ((List<Person>)Session["Person"]).Where(x => x.ID == id).FirstOrDefault();
+
+            return View(model);
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Person model)
+        {
+            try
+            {
+                if (Session["Person"] == null)
+                    Session["Person"] = new List<Person>();
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = ((List<Person>)Session["Person"]).Where(x => x.ID == id).FirstOrDefault();
+
+            return View(model);
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Person model)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                ((List<Person>)Session["Person"]).RemoveAll(r => r.ID == id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcMovie/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public bool IsTecsup { get; set; }
+    }
+}
